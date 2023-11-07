ffd6c51914565598b068d86b71308322cc6f9d82
Gradiyanto Putera Husein <gradiph@gmail.com>
2023-11-07 21:55:27 +0700

    install @nuxtjs/tailwindcss 6.9.4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/eslint-module'
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss'
   ]
 })
